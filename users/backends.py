f1f7570034e98ebe25f56bfc84dae4483c812f1a
dav_pr <dav_pr@tutanota.com>
2023-04-04 20:08:26 +0300

    docs srting backends.py

--- users/backends.py
+++ users/backends.py
@@ -3,6 +3,23 @@ from django.contrib.auth.backends import ModelBackend
 
 
 class EmailUsernameAuthenticateBackend(ModelBackend):
+    """Backend для аутентифікації користувачів за email або username.
+
+    Методи:
+    - authenticate: аутентифікація користувача за email або username та паролем.
+
+    Аргументи authenticate:
+    - request: запит, що надходить від користувача.
+    - username: ім'я користувача (якщо використовується).
+    - password: пароль користувача.
+    - email: електронна пошта користувача (якщо використовується).
+    - **kwargs: додаткові аргументи.
+
+    Повертає:
+    - User: об'єкт користувача, якщо аутентифікація пройшла успішно.
+    - None: якщо аутентифікація не вдалась.
+    """
+
     def authenticate(
         self,
         request,
@@ -11,6 +28,19 @@ class EmailUsernameAuthenticateBackend(ModelBackend):
         email=None,
         **kwargs,
     ):
+        """Аутентифікація користувача за email або username та паролем.
+
+        Аргументи:
+        - request: запит, що надходить від користувача.
+        - username: ім'я користувача (якщо використовується).
+        - password: пароль користувача.
+        - email: електронна пошта користувача (якщо використовується).
+        - **kwargs: додаткові аргументи.
+
+        Повертає:
+        - User: об'єкт користувача, якщо аутентифікація пройшла успішно.
+        - None: якщо аутентифікація не вдалась.
+        """
         user_model = get_user_model()
 
         try:
