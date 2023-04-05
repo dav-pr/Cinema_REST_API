fc3f3bf8778556547a9281eca42d56e6ad855d88
dav_pr <dav_pr@tutanota.com>
2023-04-05 16:15:59 +0300

    docs srting add cinema_hall/views.py

--- cinema_hall/models.py
+++ cinema_hall/models.py
@@ -1,8 +1,40 @@
+"""Модуль, що визначає моделі бази даних для додатку cinema."""
+
 from django.db import models
+
 from phonenumber_field.modelfields import PhoneNumberField
+"""
+PhoneNumberField є полем моделі в Django, яке дозволяє зберігати телефонні номери у відповідному форматі.
+Модуль phonenumber_field реалізує PhoneNumberField використовуючи бібліотеку Python з назвою phonenumbers,
+яка надає функціональність для перевірки та маніпулювання телефонними номерами.
+Щоб використовувати PhoneNumberField, спочатку потрібно встановити бібліотеку phonenumbers, виконавши наступну команду:
+pip install phonenumbers
+"""
 
 
 class Cinema(models.Model):
+    """
+    Модель, що представляє кінотеатр.
+
+    Атрибути:
+    ----------
+    name : CharField Назва кінотеатру.
+    address : CharField Адреса кінотеатру.
+    phone_number : PhoneNumberField Номер телефону кінотеатру.
+    facebook_social_link : URLField Посилання на сторінку кінотеатру в Facebook.
+    instagram_social_link : URLField Посилання на сторінку кінотеатру в Instagram.
+    youtube_social_link : URLField Посилання на сторінку кінотеатру в YouTube.
+
+    Клас Meta:
+    -----------
+    unique_together : tuple Кортеж полів, що повинні бути унікальними разом.
+
+    Методи:
+    -------
+    __str__() -> str
+    Повертає рядок з назвою кінотеатру.
+    """
+
     name = models.CharField(
         max_length=128,
     )
@@ -27,16 +59,37 @@ class Cinema(models.Model):
     )
 
     class Meta:
+        """Метадані."""
+
         unique_together = (
             "name",
             "address",
         )
 
     def __str__(self):
+        """Повертає рядок з назвою кінотеатру."""
         return self.name
 
 
 class Hall(models.Model):
+    """
+    Модель, що представляє зал в кінотеатрі.
+
+    Атрибути:
+    ----------
+    cinema : ForeignKey
+    Кінотеатр, якому належить зал.
+    name : CharField Назва залу.
+
+    Клас Meta:
+    -----------
+    unique_together : tuple Кортеж полів, що повинні бути унікальними разом.
+
+    Методи:
+    -------
+    __str__() -> str Повертає рядок з назвою залу.
+    """
+
     cinema = models.ForeignKey(
         Cinema,
         related_name="halls",
@@ -48,16 +101,32 @@ class Hall(models.Model):
     )
 
     class Meta:
+        """Метадані."""
+
         unique_together = (
             "name",
             "cinema",
         )
 
     def __str__(self):
+        """Повертає рядок з назвою залу."""
         return self.name
 
 
 class Seat(models.Model):
+    """
+    Цей клас Seat є моделлю Django, що представляє місце в залі кінотеатру.
+
+    Він містить наступні атрибути:
+    hall: зв'язок з залом, до якого належить місце.
+    row: ціле число, що вказує на ряд, в якому знаходиться місце.
+    number: ціле число, що вказує на номер місця у ряду.
+    Клас Meta містить визначення unique_together, що говорить, що поля hall, row і number повинні бути
+    унікальними разом.
+
+    Метод __str__() повертає рядок у вигляді "<ряд - номер>" для даного місця.
+    """
+
     hall = models.ForeignKey(
         Hall,
         on_delete=models.CASCADE,
@@ -67,6 +136,8 @@ class Seat(models.Model):
     number = models.PositiveIntegerField()
 
     class Meta:
+        """Метадані."""
+
         unique_together = (
             "hall",
             "row",
@@ -74,4 +145,5 @@ class Seat(models.Model):
         )
 
     def __str__(self):
+        """Повертає рядок з характеристиками місця."""
         return f"<{self.row} - {self.number}>"
--- cinema_hall/views.py
+++ cinema_hall/views.py
@@ -1,12 +1,19 @@
-from rest_framework.viewsets import GenericViewSet
+"""
+Модуль cinema_hall/views.py.
+
+Класи та функції в модулі cinema_hall/views.py використовуються для взаємодії з API для кінотеатру, а саме з моделями
+Cinema та Hall.
+"""
+
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAdminUser
+from rest_framework.viewsets import GenericViewSet
 
+from cinema_hall.models import Cinema, Hall
 from cinema_hall.serializers import (
     CinemaModelSerializer,
     HallModelSerializer,
 )
-from cinema_hall.models import Cinema, Hall
 
 
 class CinemaViewSet(
@@ -15,6 +22,26 @@ class CinemaViewSet(
     RetrieveModelMixin,
     GenericViewSet,
 ):
+    """
+    клас CinemaViewSet.
+
+    Цей клас є підкласом GenericViewSet, який включає в себе кілька міксинів: CreateModelMixin, ListModelMixin та
+    RetrieveModelMixin. Використовуючи цей клас, можна створити ендпойнти для створення, списку та отримання даних
+    про кінотеатри.
+
+    Атрибути:
+    queryset: Запит, що вибирає всі кінотеатри.
+    serializer_class: Клас серіалізатора, який використовується для серіалізації/десеріалізації моделей.
+    permission_classes: Список класів, які перевіряють дозвіл на доступ до ресурсу.
+
+    Методи:
+    create(self, request, *args, **kwargs): Створює новий кінотеатр на основі наданих даних у запиті.
+    list(self, request, *args, **kwargs): Повертає список усіх кінотеатрів.
+    retrieve(self, request, *args, **kwargs): Повертає інформацію про окремий кінотеатр за заданим ідентифікатором.
+    Даний клас підлягає перевірці дозволу на доступ до ресурсу, що робить його доступним лише для користувачів
+    з правами адміністратора.
+    """
+
     queryset = Cinema.objects.all()
     serializer_class = CinemaModelSerializer
     permission_classes = [
@@ -28,6 +55,57 @@ class HallViewSet(
     RetrieveModelMixin,
     GenericViewSet,
 ):
+    """
+    Клас HallViewSet.
+
+    Клас HallViewSet є нащадком класів  GenericViewSet, CreateModelMixin, ListModelMixin, RetrieveModelMixin,
+    GenericViewSet, та містить методи для створення, спискування та отримання даних для сутності Hall.
+    Він також містить permission_classes, які обмежують доступ до даних для залогінених адміністраторів.
+
+    GenericViewSet є класом перегляду Django REST Framework, який забезпечує базовий інтерфейс для реалізації
+    переглядів API. Він поєднує дві інших класи перегляду: APIView, який забезпечує розширення функціональності Django,
+    і ViewSet, який забезпечує підтримку дій CRUD (створення, отримання, оновлення та видалення) для моделі.
+    Клас GenericViewSet надає методи, такі як list(), create(), retrieve(), update(), partial_update() та destroy(),
+    які можна використовувати для реалізації переглядів API залежно від потреб проекту. Він також забезпечує підтримку
+    різних типів запитів HTTP, таких як GET, POST, PUT, PATCH та DELETE.
+
+    CreateModelMixin - це mixin (змішувач) для Django REST Framework, який додає підтримку HTTP запитів для створення
+    ресурсів. Змішувач додає реалізацію методу create для APIView, який дозволяє створювати нові записи у базі даних на
+    основі даних, які надсилаються у POST запиті.
+    Для використання CreateModelMixin потрібно додати його до класу відображення (view), який використовується в
+    Django REST Framework, а також встановити параметр serializer_class, який вказує, який серіалізатор повинен
+    використовуватися для створення нових ресурсів.
+    CreateModelMixin також додає можливість налаштувати логіку після успішного створення ресурсу за допомогою методу
+    perform_create.
+
+    ListModelMixin - це клас, що надає методи для списку об'єктів моделі (моделей), що підтримує CRUD (створення,
+    отримання, оновлення, видалення).
+    Цей клас містить два методи:
+    list(self, request, *args, **kwargs): повертає список об'єктів моделі, представлених у вигляді JSON або іншого
+    формату відповідно до запиту клієнта.
+    get_queryset(self): повертає запит на вибірку всіх об'єктів моделі, які відображаються в методі list.
+    ListModelMixin не надає методи для створення, оновлення та видалення об'єктів моделі, він тільки додає можливість
+    перегляду списку об'єктів. Для цього потрібно використовувати спеціальні класи, які додають ці функції до
+    ListModelMixin, наприклад CreateModelMixin, UpdateModelMixin, DestroyModelMixin тощо.
+
+    RetrieveModelMixin є одним з міксінів в Django REST Framework, який дозволяє додаткові операції над моделлю,
+    такі як отримання окремого запису. Клас додає HTTP метод GET, який дозволяє отримати детальну інформацію про об'єкт
+    з використанням ідентифікатора запису в URL.
+    RetrieveModelMixin вимагає визначення queryset та serializer_class, а також передається в GenericAPIView, щоб
+    включити HTTP GET запит для одного об'єкту в API. Він також включає додаткові методи для забезпечення коректної
+    поведінки серіалізатора та обробки помилок.
+
+
+    Атрибути:
+    queryset: запит на отримання всіх залів.
+    serializer_class: серіалізатор, який використовується для конвертації даних у формат JSON та навпаки.
+    permission_classes: список дозволених класів перевірки прав доступу.
+    Методи:
+    create(): створення нового об'єкту Hall на основі переданих даних.
+    list(): отримання списку всіх залів.
+    retrieve(): отримання конкретного залу за його унікальним ідентифікатором.
+    """
+
     queryset = Hall.objects.all()
     serializer_class = HallModelSerializer
     permission_classes = [
